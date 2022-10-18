617258e903d403f19c4bdd8141911702298797cb
Fernando Mavec <fermavec@gmail.com>
2022-10-18 12:12:12 -0500

    Clase 05

--- Clase 01/Homework/myqueryforhw.sql
+++ Clase 01/Homework/myqueryforhw.sql
@@ -968,57 +968,117 @@ FROM producto
 WHERE IdProducto IN (SELECT distinct IdProducto FROM fact_venta);
 
 /*Clase 05 HW*/
--- 1 Crear una tabla que permita realizar el seguimiento de los usuarios que ingresan nuevos registros en fact_venta.
-DROP TABLE IF EXISTS fact_venta_auditoria;
-CREATE TABLE fact_venta_auditoria (
-	Fecha				DATE, 
-    Fecha_Entrega 		DATE,
-    IdSucursal				INTEGER, 
-    IdCliente			INTEGER,
-    IdEmpleado			INTEGER,
-    IdProducto			INTEGER, 
-    Usuario				VARCHAR(50),
-    Fecha_Actualizacion	DATETIME
+use henry_m3_hw;
+
+-- Creamos la tabla que auditará a los usuarios que realizan cambios
+DROP TABLE IF EXISTS `fact_venta_auditoria`;
+CREATE TABLE IF NOT EXISTS `fact_venta_auditoria` (
+	IdFactVentaAuditoria INT NOT NULL AUTO_INCREMENT,
+    `Fecha`				DATE,
+	`Fecha_Entrega`		DATE,
+  	`IdCanal` 			INTEGER,
+  	`IdCliente` 		INTEGER,
+  	`IdEmpleado` 		INTEGER,
+  	`IdProducto` 		INTEGER,
+    `usuario` 			VARCHAR(30),
+    `fechaModificacion` 	DATETIME,
+    PRIMARY KEY(IdFactVentaAuditoria)
 );
 
-SELECT * FROM fact_venta_auditoria;
-
--- 2. Crear una acción que permita la carga de datos en la tabla anterior.alter
-DROP TRIGGER IF EXISTS fact_venta_audit;
-CREATE TRIGGER fact_venta_audit 
-AFTER INSERT ON aux_venta
+DROP TRIGGER fact_venta_auditoria;
+CREATE TRIGGER fact_venta_auditoria AFTER INSERT ON fact_venta
 FOR EACH ROW
-INSERT INTO fact_venta_auditoria (Fecha, Fecha_Entrega, IdSucursal, IdCliente, IdEmpleado, IdProducto, Usuario, Fecha_Actualizacion)
-VALUES (NEW.Fecha, NEW.Fecha_Entrega, NEW.IdSucursal, NEW.IdCliente, NEW.IdEmpleado, NEW.IdProducto, CURRENT_USER, NOW());
+INSERT INTO fact_venta_auditoria (Fecha, Fecha_Entrega, IdCanal, IdCliente, IdEmpleado,
+ IdProducto, usuario, fechaModificacion)
+VALUES (NEW.Fecha, NEW.Fecha_Entrega, NEW.IdCanal, NEW.IdCliente, NEW.IdEmpleado,
+ NEW.IdProducto, current_user(), NOW());
 
-INSERT INTO aux_venta
-VALUES (18255, '2022-10-17', '2022-10-17', 1185, 13, 1274, 42779, 955.00, 1, 0);
-INSERT INTO aux_venta
-VALUES (18255, '2001-10-17', '2022-10-17', 1185, 13, 1274, 42779, 955.00, 1, 0);
+TRUNCATE TABLE fact_venta;
 
-SELECT * FROM fact_venta_auditoria;
+INSERT INTO fact_venta
+SELECT IdVenta, Fecha, Fecha_Entrega, IdCanal, IdCliente, IdEmpleado, IdProducto, Precio, Cantidad
+FROM venta
+LIMIT 30;
 
--- 3
-DROP TABLE IF EXISTS count_venta_auditoria;
-CREATE TABLE count_venta_auditoria (
-	IdConteo INTEGER NOT NULL AUTO_INCREMENT,
-    Conteo INTEGER,
-    Usuario VARCHAR(50),
-    Fecha_Modificacion DATETIME,
-    PRIMARY KEY(IdConteo)
+-- Creamos la tabla que llevara una cuenta de los registros.
+DROP TABLE IF EXISTS `fact_venta_registros`;
+CREATE TABLE IF NOT EXISTS `fact_venta_registros` (
+  	IdFactVentaRegistros INT NOT NULL AUTO_INCREMENT,
+	cantidadRegistros INT,
+	usuario VARCHAR (20),
+	fechaHora DATETIME,
+	PRIMARY KEY (IdFactVentaRegistros)
 );
 
--- 4
-DROP TRIGGER IF EXISTS count_venta_audit;
-CREATE TRIGGER count_venta_audit
-AFTER INSERT ON aux_venta
+DROP TRIGGER fact_venta_registros;
+CREATE TRIGGER fact_venta_registros AFTER INSERT ON fact_venta
 FOR EACH ROW
-INSERT INTO count_venta_auditoria(conteo)
-VALUES((SELECT COUNT(*) FROM aux_venta));
+INSERT INTO fact_venta_registros (cantidadRegistros, usuario, fechaHora)
+VALUES ((SELECT count(*) FROM fact_venta), current_user(), now());
 
-SELECT * FROM count_venta_auditoria;
+-- Creamos una tabla donde podremos almacenar la cantidad de registros por día
+DROP TABLE registros_tablas;
+CREATE TABLE registros_tablas (
+idRegistros INT NOT NULL AUTO_INCREMENT,
+tabla VARCHAR(30),
+fechaHora DATETIME,
+cantidadRegistros INT,
+PRIMARY KEY (idRegistros)
+);
 
-INSERT INTO aux_venta
-VALUES (18255, '2022-10-17', '2022-10-17', 1185, 13, 1274, 42779, 955.00, 1, 0);
+INSERT INTO registros_tablas (tabla, fechaHora, cantidadRegistros)
+SELECT 'venta', NOW(), COUNT(*)
+FROM venta;
 
-SELECT * FROM count_venta_auditoria;
+INSERT INTO registros_tablas (tabla, fechaHora, cantidadRegistros)
+SELECT 'gasto', NOW(), COUNT(*)
+FROM gasto;
+
+-- Creamos una tabla para auditar cambios
+DROP TABLE IF EXISTS `fact_venta_cambios`;
+CREATE TABLE IF NOT EXISTS `fact_venta_cambios` (
+  	IdFactVentaCambios INT NOT NULL AUTO_INCREMENT,
+    `Fecha` 			DATE,
+  	`IdCliente` 		INTEGER,
+  	`IdProducto` 		INTEGER,
+    `Precio` 			DECIMAL(15,3),
+    `Cantidad` 			INTEGER,
+    `usuario` 			VARCHAR(20),
+    `fechaHoraModificacion` 	DATETIME,
+    PRIMARY KEY(IdFactVentaCambios)
+);
+
+DROP TRIGGER control_actualizacion;
+CREATE TRIGGER control_actualizacion AFTER UPDATE ON fact_venta
+FOR EACH ROW
+INSERT INTO fact_venta_cambios
+VALUES (NULL, OLD.Fecha, OLD.IdCliente, OLD.IdProducto, OLD.Precio, OLD.Cantidad, current_user(), NOW());
+
+-- Bonus Track Tabla para eliminados (cuidado con eliminar datos sin un control estricto)
+
+DROP TABLE IF EXISTS `fact_venta_eliminados`;
+CREATE TABLE IF NOT EXISTS fact_venta_eliminados (
+  	IdFactVentaEliminados INT NOT NULL AUTO_INCREMENT,
+    `Fecha` 			DATE,
+  	`IdCliente` 		INTEGER,
+  	`IdProducto` 		INTEGER,
+    `Precio` 			DECIMAL(15,3),
+    `Cantidad` 			INTEGER,
+    `usuario` 			VARCHAR(20),
+    `fechaHoraModificacion` 	DATETIME,
+    PRIMARY KEY(IdFactVentaEliminados)
+);
+
+DROP TRIGGER control_eliminados;
+CREATE TRIGGER control_eliminados AFTER DELETE ON fact_venta
+FOR EACH ROW
+INSERT INTO fact_venta_eliminados
+VALUES (NULL, OLD.Fecha, OLD.IdCliente, OLD.IdProducto, OLD.Precio, OLD.Cantidad, current_user(), NOW());
+
+UPDATE fact_venta
+SET Cantidad = 2
+WHERE IdVenta = 2;
+
+DELETE
+FROM fact_venta
+WHERE IdVenta = 2;
