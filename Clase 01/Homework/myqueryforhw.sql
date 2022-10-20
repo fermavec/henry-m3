dac4dbbf6fad617552386107ea25c951d3ba1491
Fernando Mavec <fermavec@gmail.com>
2022-10-20 12:17:11 -0500

    Clase 07

--- Clase 01/Homework/myqueryforhw.sql
+++ Clase 01/Homework/myqueryforhw.sql
@@ -1172,69 +1172,89 @@ WHERE Rango_Etario  = @grupo_etario;
 
 /*Clase 07*/
 -- 1
-SELECT cliente.Nombre_y_Apellido AS nombre, 
-venta.IdProducto AS Producto, 
-venta.Precio AS Precio
-FROM cliente
-INNER JOIN venta
-ON (venta.IdCliente = cliente.IdCliente);
+use henry_m3_hw;
 
--- 2
-SELECT cliente.Nombre_y_Apellido AS nombre, 
-SUM(venta.Cantidad) AS ProductosComprados,
-venta.IdProducto AS Producto
-FROM cliente
-LEFT JOIN venta
-ON (venta.IdCliente = cliente.IdCliente)
-GROUP BY venta.IdProducto
-ORDER BY ProductosComprados ASC;
+-- #1
+SELECT DISTINCT c.IdCliente, c.Nombre_y_Apellido, v.IdProducto, v.Precio
+FROM venta v
+LEFT JOIN cliente c using(IdCliente);
 
--- 3
-SELECT cliente.Nombre_y_Apellido AS nombre, 
-SUM(venta.Cantidad) AS ProductosComprados,
-YEAR(venta.Fecha) AS Anio
-FROM cliente
-LEFT JOIN venta
-ON (venta.IdCliente = cliente.IdCliente)
-GROUP BY YEAR(venta.Fecha)
-ORDER BY ProductosComprados ASC;
+-- #2
+SELECT c.IdCliente, c.Nombre_y_Apellido,
+		SUM(v.Cantidad) as cantidad_productos_null,
+        SUM(ifnull(v.Cantidad, 0)) as cantidad_productos
+FROM cliente c
+LEFT JOIN venta v using(IdCliente)
+GROUP BY c.IdCliente, c.Nombre_y_Apellido
+ORDER BY cantidad_productos DESC;
 
--- 4
-SELECT cliente.Nombre_y_Apellido AS nombre, 
-SUM(venta.Cantidad) AS ProductosComprados,
-venta.IdProducto AS Producto, 
-ROUND(AVG(venta.Precio), 2) AS PrecioPromedio
-FROM cliente
-INNER JOIN venta
-ON (venta.IdCliente = cliente.IdCliente)
-GROUP BY venta.IdProducto
-ORDER BY ProductosComprados DESC;
+-- #3
+SELECT c.IdCliente, c.Nombre_y_Apellido, YEAR(v.Fecha), count(v.IdVenta) as total_compras
+FROM venta v
+JOIN cliente c using(IdCliente)
+GROUP BY c.IdCliente, c.Nombre_y_Apellido, YEAR(v.Fecha)
+ORDER BY YEAR(v.Fecha) DESC;
 
--- 5
-SELECT SUM((venta.Precio * venta.Cantidad)) AS VentasTotales, 
-SUM(venta.Cantidad) AS ProductosVendidos, 
-localidad.Localidad AS Localidad
-FROM venta
-INNER JOIN sucursal
-ON (sucursal.IdSucursal = venta.IdSucursal)
-INNER JOIN localidad
-ON (localidad.IdLocalidad = sucursal.IdLocalidad)
-GROUP BY Localidad
-ORDER BY VentasTotales DESC;
+-- #4
+SELECT c.IdCliente, c.Nombre_y_Apellido, p.Producto, p.IdProducto,
+		SUM(v.Cantidad) as cantidad_productos,
+        ROUND(AVG(v.Precio), 2) as precio_promedio
+FROM venta v
+JOIN producto p using(IdProducto)
+JOIN cliente c using(IdCliente)
+GROUP BY c.IdCliente, c.Nombre_y_Apellido, p.Producto, p.IdProducto
+ORDER BY c.IdCliente;
 
--- 6
-SELECT SUM((venta.Precio * venta.Cantidad)) AS VentasTotales, 
-SUM(venta.Cantidad) AS ProductosVendidos, 
-provincia.Provincia AS Provincia
-FROM venta
-INNER JOIN sucursal
-ON (sucursal.IdSucursal = venta.IdSucursal)
-INNER JOIN localidad
-ON (localidad.IdLocalidad = sucursal.IdLocalidad)
-INNER JOIN provincia
-ON (provincia.IdProvincia = localidad.IdProvincia)
-GROUP BY Provincia
-ORDER BY VentasTotales DESC;
+-- #5
+-- Revisar duplicados
+SELECT localidad 
+FROM localidad
+GROUP BY localidad
+HAVING count(*) > 1;
+
+SELECT p.Provincia, l.Localidad,
+		SUM(v.Cantidad) AS cantidad_productos,
+        SUM(v.Precio * v.Cantidad) as total_ventas,
+        count(v.IdVenta) as volumen_venta
+FROM venta v
+LEFT JOIN cliente c using(IdCliente)
+LEFT JOIN localidad l ON(l.IdLocalidad = c.IdLocalidad)
+LEFT JOIN provincia p using(IdProvincia)
+WHERE v.Outlier = 1
+GROUP BY p.Provincia, l.Localidad
+ORDER BY p.Provincia, l.Localidad;
+
+-- #6
+SELECT p.Provincia,
+		SUM(v.Cantidad) AS cantidad_productos,
+        SUM(v.Precio * v.Cantidad) as total_ventas,
+        count(v.IdVenta) as volumen_venta
+FROM venta v
+LEFT JOIN cliente c using(IdCliente)
+LEFT JOIN localidad l ON(l.IdLocalidad = c.IdLocalidad)
+LEFT JOIN provincia p using(IdProvincia)
+WHERE v.Outlier = 1
+GROUP BY p.Provincia
+HAVING total_ventas > 100000
+ORDER BY total_ventas desc;
+
+-- #7
+SELECT c.Rango_Etario,
+		SUM(v.Cantidad) AS cantidad_productos,
+        SUM(v.Precio * v.Cantidad) as total_ventas
+FROM venta v
+JOIN cliente c using(IdCliente)
+WHERE v.Outlier = 1
+GROUP BY c.Rango_Etario
+ORDER BY c.Rango_Etario;
+
+-- #8
+SELECT p.IdProvincia, p.Provincia, count(c.IdCliente) as cantidad_clientes
+FROM provincia p
+LEFT JOIN localidad l ON(l.IdProvincia = p.IdProvincia)
+LEFT JOIN cliente c ON(c.IdLocalidad = l.IdLocalidad)
+GROUP BY p.IdProvincia, p.Provincia
+ORDER BY p.IdProvincia;
 
 
 
